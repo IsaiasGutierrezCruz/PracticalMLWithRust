185acdba978e95bbee5c8147622ec60d76b630c8
Isaias Gutierrez Cruz <isaiascruz@comunidad.unam.mx>
2024-05-05 22:17:18 -0600

    First commit

---
+++ myfirstapp/src/main.rs
@@ -0,0 +1,22 @@
+// #![feature(core_intrinsics)]
+
+// fn print_type_of<T>(_: &T){
+//     println!(
+//         "{}",std::intrinsics::type_name::<T>()
+//     );
+// }
+
+// fn main() {
+//     println!("Hello, world!");
+
+//     let x = "learning rust";
+
+//     println!("{}", x);
+
+//     print_type_of(&x);
+// }
+
+// write a hello world program
+fn main() {
+    println!("Hello, world!");
+}
